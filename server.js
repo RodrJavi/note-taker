4742f9a9b0bef905adbddcc59cac11c2d24339a1
Javi <nb2javi@gmail.com>
2023-09-04 12:06:24 -0500

    New fetch requests properly showing new notes

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
-app.get("/api/notes", (req, res) => res.json(notes));
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/notes.json", "utf8", (err, data) => {
+    const parsedNotes = JSON.parse(data);
+    res.json(parsedNotes);
+  });
+});
 
 app.post("/api/notes", (req, res) => {
   console.info(`${req.method} request received to add a note`);
